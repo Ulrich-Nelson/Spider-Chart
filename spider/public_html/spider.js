2238f3b2d2a83e36ba2b78ffb5517f5cceadc7c8
Christophe Magnani <christophe.magnani@gmail.com>
2018-05-29 23:18:31 +0200

    Update polygon

--- spider/public_html/spider.js
+++ spider/public_html/spider.js
@@ -2,30 +2,45 @@ function pentagone()
 {
     let canvas = document.getElementById("pointeur");
     let context = canvas.getContext("2d");
-    // dessiner un pentagone regulier
-    let numberOfSides = 5,
-            size = 200,
-            Xcenter = 205,
-            Ycenter = 205;
 
-    context.beginPath( );
-    context.moveTo(Xcenter + size * Math.cos(0), Ycenter + size * Math.sin(0));
-    // TODO tableau des coordonnées des normes
-    //      norm_x[i] et norm_y[i]
-    //      calculé avec une boucle et les cos et sin ci-dessous
-    // TODO tableau des coefficients du patient : à la main (a,b,c,d,e)
-    // TODO tableau des coordonnées cibles du patient
-    //      target_x[i] et target_y[i]
-    //      faire une boucle avec la formule
-    // TODO dessiner le pentagone des normes + le polygone du patient
-    for(let i = 1; i <= numberOfSides; i += 1)  {
-        context.lineTo(Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    let numberOfSides = 5;
+    let size = 200;
+    let Xcenter = 205;
+    let Ycenter = 205;
+
+    // Compute norm coordinates
+    let norm_x = [];
+    let norm_y = [];
+    for (let i = 0; i <= numberOfSides; i += 1) {
+        norm_x[i] = Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides);
+        norm_y[i] = Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides);
     }
 
+    // TODO DEFINIR LE TABLEAU DES COEFFICIENTS a,b,c,d,e
+
+    // TODO CALCULER LES COORDONNÉES DU PATIENT DANS UNE BOUCLE
+    //      target_x[i] , target_y[i]
+    //      UTILISER LA FORMULE
+    //      xa - xO = k * (xA - xO)
+    //      ya - yO = k * (yA - yO)
+
+    // Draw pentagon of norms
+    context.beginPath();
+    context.moveTo(norm_x[0], norm_y[0]);
+    for (let i = 1; i <= numberOfSides; i += 1) {
+        context.lineTo(norm_x[i], norm_y[i]);
+    }
     context.strokeStyle = "black";
     context.lineWidth = 1;
     context.stroke();
-    }
+
+    // TODO DESSINER LE PENTAGONE DU PATIENT
+
+    // Draw center
+    context.beginPath();
+    context.fillStyle = "black";
+    context.arc(Xcenter, Ycenter, 2, 0, 2 * Math.PI);
+    context.fill();
+}
 
 window.onload = pentagone;
-
