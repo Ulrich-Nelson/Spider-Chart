9b03efe3db6ac7a4c47ad40355c5475792370c34
Christophe Magnani <christophe.magnani@gmail.com>
2018-06-08 22:10:03 +0200

    Draw labels

--- spider/public_html/spider.js
+++ spider/public_html/spider.js
@@ -1,5 +1,6 @@
-function SpiderChart(canvas, coefficients) {
+function SpiderChart(canvas, labels, coefficients) {
     this.canvas = canvas;
+    this.labels = labels;
     this.coefficients = coefficients;
 }
 
@@ -7,24 +8,33 @@ SpiderChart.prototype.display = function () {
     let context = this.canvas.getContext("2d");
 
     let numberOfSides = this.coefficients.length;
-    let size = 200;
-    let Xcenter = 205;
-    let Ycenter = 205;
+    let radius = 200;
+    let labelExtent = 10;
+    let centerX = 300;
+    let centerY = 300;
+
+    // Compute label coordinates
+    let label_x = [];
+    let label_y = [];
+    for (let i = 0; i < numberOfSides; i += 1) {
+        label_x[i] = centerX + (labelExtent + radius) * Math.cos(i * 2 * Math.PI / numberOfSides);
+        label_y[i] = centerY + (labelExtent + radius) * Math.sin(i * 2 * Math.PI / numberOfSides);
+    }
 
     // Compute norm coordinates
     let norm_x = [];
     let norm_y = [];
     for (let i = 0; i <= numberOfSides; i += 1) {
-        norm_x[i] = Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides);
-        norm_y[i] = Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides);
+        norm_x[i] = centerX + radius * Math.cos(i * 2 * Math.PI / numberOfSides);
+        norm_y[i] = centerY + radius * Math.sin(i * 2 * Math.PI / numberOfSides);
     }
 
     // Compute target coordinates
     let target_x = [];
     let target_y = [];
     for (let i = 0; i < numberOfSides; i += 1) {
-        target_x[i] = this.coefficients[i] * (norm_x[i] - Xcenter) + Xcenter;
-        target_y[i] = this.coefficients[i] * (norm_y[i] - Ycenter) + Ycenter;
+        target_x[i] = this.coefficients[i] * (norm_x[i] - centerX) + centerX;
+        target_y[i] = this.coefficients[i] * (norm_y[i] - centerY) + centerY;
     }
     target_x[numberOfSides] = target_x[0];
     target_y[numberOfSides] = target_y[0];
@@ -52,6 +62,15 @@ SpiderChart.prototype.display = function () {
     // Draw center
     context.beginPath();
     context.fillStyle = "black";
-    context.arc(Xcenter, Ycenter, 2, 0, 2 * Math.PI);
+    context.arc(centerX, centerY, 2, 0, 2 * Math.PI);
     context.fill();
+
+    // Draw labels
+    context.beginPath();
+    context.font = "20px Arial";
+    context.textBaseline = "middle";
+    for (let i = 0; i < numberOfSides; i += 1) {
+        context.textAlign = label_x[i] < centerX ? "right" : "left";
+        context.fillText(this.labels[i], label_x[i], label_y[i]);
+    }
 };
