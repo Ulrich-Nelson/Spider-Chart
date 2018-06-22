e3226ec348a8cb56cb29c1a2a62ffb90bda39ff6
Christophe Magnani <christophe.magnani@gmail.com>
2018-06-22 16:13:37 +0200

    Fix bug

--- spider/public_html/server.js
+++ spider/public_html/server.js
@@ -2,7 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Salut tout le monde !');
     res.write('hello tout le monde, ceci est mon premier serveur créer');
     res.end();
     console.log('serveur lancé sur le port 5000.........');
